6f350f53c2def32c289a9b05ac313a63960de8a9
Aafiya choudhary <119157685+Aafiya-01@users.noreply.github.com>
2023-09-05 15:37:40 +0530

    Add files via upload

---
+++ cancer cell.py
@@ -0,0 +1,11 @@
+# importing the Python module
+import sklearn
+
+# importing the dataset
+from sklearn.datasets import load_breast_cancer
+data = load_breast_cancer()
+label_names = data['target_names']
+labels = data['target']
+feature_names = data['feature_names']
+features = data['data']
+print(label_names)
